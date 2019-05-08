f7070bef417770b056c1c8f0de7fa9723e1f1b16
Coop Digital Tech <coopdigitaltech@Coops-MacBook-Pro.local>
2019-05-08 09:04:32 +0100

    Added the ability to add income and expenses to the UI. Input fields also clear and refocus when an income or expense is added to the UI

--- RossVersion/app.js
+++ RossVersion/app.js
@@ -20,12 +20,40 @@ var budgetController = (function() {
     
     allItems: {
       exp: [], 
-      inc: []
+      inc: [],
+      id: []
     },
     
     totals: {
       exp: 0,
       inc: 0
+    }
+  }
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      
+      // Create new ID
+      if(data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp'
+      if(type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    
+    testing: function() {
+      console.log(data);
     }
   }
 
@@ -40,7 +68,10 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
+
   }
 
   return {
@@ -51,12 +82,56 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       }
-      
     }, 
+
+    addListItem: function(obj, type) {
+
+      var html, newHtml, element;
+
+      // Create HTML string with placeholder text.
+
+      if(type === 'inc') {
+        element = DOMstrings.incomeContainer;
+
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+      } else if (type === 'exp') {
+        element = DOMstrings.expenseContainer;
+
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace the placeholder text with some actual data. 
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      // Insert the HTML into the DOM 
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
+
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, arr) {
+        current.value = "";
+      })
+
+      fieldsArr[0].focus();
+
+    },
 
     getDOMstrings: function() {
       return DOMstrings
     }
+
+    
   }
 
 })();
@@ -80,16 +155,20 @@ var controller = (function(budgetCtrl, UICtrl) {
  
   
   var ctrlAddItem = function () {
+
+    var input, newItem;
    
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
     // 2. Add the item to the budget controller
-
+    newItem = budgetController.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
+    // 4. Clear the fields 
+    UICtrl.clearFields();
+    // 5. Calculate the budget
 
-    // 4. Calculate the budget
-
-    // 5. Display the budget
+    // 6. Display the budget
     
 
   }
